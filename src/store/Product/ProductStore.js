5d3ae570d99e56a20464dfc900c68854561cfd90
aleprosli <aliff.rosli96@gmail.com>
2023-03-24 11:13:36 +0800

    fix: product store

--- src/store/Product/ProductStore.js
+++ src/store/Product/ProductStore.js
@@ -5,23 +5,28 @@ const state = {
 }
 
 const getters = {
+    //amik cons state dan state.data
     data: state => state.data
 }
 
 const mutations = {
-    setData(state,data) {
-        state.data = data
+    //nama function dgn parameter refer dalam commit
+    setData(state,responseData) {
+        state.data = responseData
     }
 }
 
 const actions = {
     async getProductCategory({commit}) {
+        //import productservices.nama function
         let response = await productServices.getProductCategory()
         console.log('response', response)
+        //commit mutation
         commit('setData',response.data)
     }
 }
 
+//export default 
 export default {
     state,
     getters,
