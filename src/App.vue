131cbd316f3a3b033922092d099ab4bf8afb7f35
aleprosli <aliff.rosli96@gmail.com>
2023-03-24 10:29:30 +0800

    add simple flow to fetch data

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,44 @@ import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
+<script>
+//npm install axios
+import axios from 'axios'
+
+//export ni defauly
+export default {
+  data(){
+    return {
+      params: [
+
+      ]
+    }
+  },
+
+//then kena ada method
+  methods: {
+    //sini declare nama function 
+    getProductCategory(){
+      //panggil axios get url , lps response, then -> kita nak buat apa
+      axios.get('https://dummyjson.com/products/categories').then(response => {
+        //this.params refer array dalam data di atas. response.data kita tgk dalam output yg di fetch dlm console contoh
+        //response = [data => []]
+        this.params = response.data
+        console.log('response', this.params)
+      })
+    }
+  },
+
+  //kena mounted
+  mounted(){
+    //amik nama function yg kita nak declare
+    this.getProductCategory()
+  }
+}
+</script>
+
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+   <li v-for="item in params">{{ item }}</li>
 </template>
 
 <style scoped>
